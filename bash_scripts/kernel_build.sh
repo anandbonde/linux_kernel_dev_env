505dd510372a2b1a8818fd12ce65aa042b7127c8
Anand Bonde <anand.bonde@gmail.com>
2021-02-20 23:18:44 -0700

    kbuild: add steps to disable debug build

--- bash_scripts/kernel_build.sh
+++ bash_scripts/kernel_build.sh
@@ -1,14 +1,17 @@
-echo "Run distclean"
+echo "Run make distclean"
 make distclean
 
 echo "Copy current kernel config."
 cp /boot/config-$(uname -r) .config
 
-echo "Make oldconfig, accept defaults"
-yes '' | make oldconfig
+echo "Make oldconfig (accept defaults)"
+yes '' | make oldconfig > /dev/null
+
+echo "Disable debug info in build"
+scripts/config --disable DEBUG_INFO
 
 echo "Set distcc hosts"
-export DISTCC_HOSTS="localhost mercury"
+export DISTCC_HOSTS="localhost venus"
 
 echo "Build"
-make CC="ccache distcc gcc" -j 4 deb-pkg LOCALVERSION=-ab-stable
+make CC="ccache distcc gcc" -j $(getconf _NPROCESSORS_ONLN) deb-pkg LOCALVERSION=-ab-stable
